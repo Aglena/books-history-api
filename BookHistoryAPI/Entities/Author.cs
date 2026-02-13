85e016c3341c3603185e690d177641a187bceebd
Aglena <agapova.eln@gmail.com>
2026-02-13 11:40:52 +0100

    added empty author name validation

--- BookHistoryAPI/Entities/Author.cs
+++ BookHistoryAPI/Entities/Author.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookHistoryApi.Entities
 {
     public class Author
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public ICollection<Book> Books { get; set; } = new List<Book>();
