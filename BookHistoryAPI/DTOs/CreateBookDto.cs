47abb92c9fcdaa087aa6d4e3f00bd261445c57a1
Aglena <agapova.eln@gmail.com>
2026-02-13 11:20:20 +0100

    added empty title validation

--- BookHistoryAPI/DTOs/CreateBookDto.cs
+++ BookHistoryAPI/DTOs/CreateBookDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookHistoryApi.DTOs
 {
     public class CreateBookDto
     {
+        [Required]
         public string Title { get; set; } = string.Empty;
         public string ShortDescription { get; set; } = string.Empty;
         public DateTime PublishDate { get; set; }
